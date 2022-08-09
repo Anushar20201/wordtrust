616107f8441fee0efd15fee9fca64ef47495e703
Anushar20201 <103870102+Anushar20201@users.noreply.github.com>
2022-08-08 21:40:56 -0500

    Update connection.js
    get db url from config

--- config/connection.js
+++ config/connection.js
@@ -6,8 +6,8 @@ require('dotenv').config();
 // Create connection to our database, pass in your MySQL information for username and password
 let sequelize;
 
-if (CLEARDB_DATABASE_URL) {
-  sequelize = new Sequelize(CLEARDB_DATABASE_URL);
+if (process.env.CLEARDB_DATABASE_URL) {
+  sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL);
 } else {
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
